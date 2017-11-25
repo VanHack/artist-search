334706017aaf77825561ab5900d02c2f113d75d2
Paulo Rodrigues <pauloandreget@gmail.com>
2017-11-25 07:04:15 -0200

    Fix datetime field

--- src/app/models/event/event.ts
+++ src/app/models/event/event.ts
@@ -6,7 +6,7 @@ export class Event implements Base {
     public id: number;
     public url: string;
     public on_sale_datetime: Date;
-    public datetimethumb_url: Date;
+    public datetime: Date;
     public venue: Venue;
     public offers: Offer[];
     public lineup: string[];

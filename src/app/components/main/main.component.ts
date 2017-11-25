eca0c0c4b99f87da3e98e9303ccbd6409a62177a
Paulo Rodrigues <pauloandreget@gmail.com>
2017-11-25 08:38:44 -0200

    Search artists from header form

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-main',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-    constructor() { }
+    constructor(
+        private router: Router,
+        private route: ActivatedRoute,
+        private formBuilder: FormBuilder
+    ) { }
+
+    public formSearch: FormGroup;
 
     ngOnInit() {
+        this.formSearch = this.formBuilder.group({
+            keyword: [this.route.snapshot.children[0].params['name'], [Validators.required]]
+        });
+    }
+
+    onSubmit() {
+        if (this.formSearch.valid == false) {
+            return;
+        }
+
+        this.router.navigate(['/artists', this.formSearch.value.keyword]);
     }
 
 }

78fea49ef09229edb47c3c7ba452b84e7eaedbab
Paulo Rodrigues <pauloandreget@gmail.com>
2017-11-25 15:39:20 -0200

    Change latitude and longitude type

--- src/app/models/event/venue.ts
+++ src/app/models/event/venue.ts
@@ -2,8 +2,8 @@ import { Base } from '../base';
 
 export class Venue implements Base {
     public name: string;
-    public latitude: number;
-    public longitude: number;
+    public latitude: string;
+    public longitude: string;
     public city: string;
     public region: string;
     public country: string;
